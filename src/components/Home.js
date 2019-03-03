02de15774b2c548ec9e0449c018e24379b79b3e6
Leandro Solidum <solidum.leandro@gmail.com>
2019-03-02 18:10:18 -0800

    build on homepage structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => <h2> Home </h2>;
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div id="home-container">
+      <section id="hero">
+        <h1>Find the Balance</h1>
+        <h2>the truth is in the spectrum</h2>
+      </section>
+      <StoryCard />
+      <StoryCard />
+      <StoryCard />
+      <section id="news-sources-container">
+        <h3>News sources we parse through</h3>
+      </section>
+    </div>
+  );
+};
+
+class StoryCard extends Component {
+  render() {
+    return (
+      <Link to="/spectrum/">
+        <div class="story-card">
+          <span>Title</span>
+        </div>
+      </Link>
+    );
+  }
+}
 
 export default Home;
