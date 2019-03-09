d2c75693ec1544585d02eb460caa4604f20455f0
Leandro Solidum <solidum.leandro@gmail.com>
2019-03-08 16:07:41 -0800

    add news source logos and story images

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import "./Home.css";
+import cnnLogo from "../images/news-sources/CNN.svg";
+import bbcLogo from "../images/news-sources/BBC.svg";
+import bloombergLogo from "../images/news-sources/Bloomberg.svg";
+import nytimesLogo from "../images/news-sources/NewYorkTimes.svg";
 
 const Home = () => {
   return (
@@ -10,19 +14,34 @@ const Home = () => {
         <h1>Find the Balance</h1>
         <h2>the truth is in the spectrum</h2>
       </section>
-      <StoryCard />
-      <StoryCard />
-      <StoryCard />
+      <StoryCard
+        title="Huawei CFO Arrest"
+        backgroundImage="https://upload.wikimedia.org/wikipedia/commons/1/14/Meng_Wanzhou_at_Russia_Calling%21_Investment_Forum.jpg"
+      />
+      <StoryCard
+        title="Government Shutdown"
+        backgroundImage="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/President_Trump_Meets_With_Congressional_Leadership_%2845966024294%29.jpg/640px-President_Trump_Meets_With_Congressional_Leadership_%2845966024294%29.jpg"
+      />
+      <StoryCard
+        title="Venezuelan Presidential Crisis"
+        backgroundImage="https://upload.wikimedia.org/wikipedia/commons/3/30/2017_Venezuelan_protests_flag.jpg"
+      />
       <section id="news-sources-container">
         <h3>News sources we parse through</h3>
         <div id="news-sources">
           <ul>
-            <li>CNN</li>
-            <li>BBC</li>
-            <li>NYTimes</li>
-            <li>Source</li>
-            <li>Source</li>
-            <li>Source</li>
+            <li>
+              <NewsLogo title="CNN" image={cnnLogo} />
+            </li>
+            <li>
+              <NewsLogo title="BBC" image={bbcLogo} />
+            </li>
+            <li>
+              <NewsLogo title="Bloomberg" image={bloombergLogo} />
+            </li>
+            <li>
+              <NewsLogo title="New York Times" image={nytimesLogo} />
+            </li>
           </ul>
         </div>
       </section>
@@ -33,13 +52,24 @@ const Home = () => {
 class StoryCard extends Component {
   render() {
     return (
-      <Link to="/spectrum/">
-        <div class="story-card">
-          <span>Title</span>
+      <Link to="/spectrum/" class="story-card-container">
+        <div
+          class="story-card"
+          style={{
+            backgroundImage: `linear-gradient(0deg,rgba(0,0,0,0),rgba(0,0,0,0.8)),url(${
+              this.props.backgroundImage
+            })`
+          }}
+        >
+          <span class="title">{this.props.title}</span>
         </div>
       </Link>
     );
   }
+}
+
+function NewsLogo(props) {
+  return <img class="news-logo" src={props.image} alt={props.title} />;
 }
 
 export default Home;
