7e02b229a65982cf458e4941e5ff61f7b8e12b91
Leandro Solidum <solidum.leandro@gmail.com>
2019-04-11 19:15:52 -0700

    hide navbar when viewing cluster

--- src/App.js
+++ src/App.js
@@ -10,32 +10,37 @@ import ClusterViz from "./components/ClusterViz";
 import logo from "./logo.png";
 
 class App extends Component {
+  NavBar() {
+    return (
+      <nav>
+        <ul>
+          <li id="nav-link-1">
+            <Link to="/">
+              <img id="logo" src={logo} alt="Symposia Logo" />
+            </Link>
+          </li>
+          <li id="nav-link-2">
+            <span hidden>
+              <Link to="/about/">About</Link>
+            </span>
+            <a
+              href="mailto:pvthejas@uw.edu,jyc24@uw.edu,aman.arya524@gmail.com,lndrgs@uw.edu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
   render() {
     return (
       <Router>
         <div id="app-container">
-          <nav>
-            <ul>
-              <li id="nav-link-1">
-                <Link to="/">
-                  <img id="logo" src={logo} alt="Symposia Logo" />
-                </Link>
-              </li>
-              <li id="nav-link-2">
-                <span hidden>
-                  <Link to="/about/">About</Link>
-                </span>
-                <a
-                  href="mailto:pvthejas@uw.edu,jyc24@uw.edu,aman.arya524@gmail.com,lndrgs@uw.edu"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </nav>
-
+          <Route path="/" exact render={this.NavBar} />
           <main>
             <Route path="/" exact component={Home} />
             <Route path="/about/" component={About} />
