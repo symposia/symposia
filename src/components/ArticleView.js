c0510a180888a5e44e0b6eaa893751123740ac83
Leandro Solidum <solidum.leandro@gmail.com>
2019-05-02 06:16:56 -0700

    add remaining data to related articles

--- src/components/ArticleView.js
+++ src/components/ArticleView.js
@@ -3,14 +3,13 @@ import "./css/ArticleView.css";
 
 class ArticleView extends Component {
   componentDidMount() {
-    console.log(this.props.article)
+    console.log(this.props.article);
   }
-
-
 
   render() {
     let article = this.props.article;
-    let author = article.authors.length > 0 ? "By " + article.authors[0].name : null
+    let author =
+      article.authors.length > 0 ? "By " + article.authors[0].name : null;
     let relatedArticles = this.props.relatedArticles;
 
     return (
@@ -21,7 +20,10 @@ class ArticleView extends Component {
         <div className="article-view-main">
           <div className="av-main-header">
             <div className="av-news-source">
-              <img src={"http://logo.clearbit.com/" + article.source.uri} alt="news source" />
+              <img
+                src={"http://logo.clearbit.com/" + article.source.uri}
+                alt="news source"
+              />
             </div>
             <div className="av-title">
               <h1>{article.title}</h1>
@@ -35,8 +37,8 @@ class ArticleView extends Component {
               <p> {author} </p>
             </div>
             <div className="concepts">
-              {article.concepts.slice(0,4).map((concept, index) => {
-                return <ArticleConcept key={index} concept={concept} />
+              {article.concepts.slice(0, 4).map((concept, index) => {
+                return <ArticleConcept key={index} concept={concept} />;
               })}
             </div>
             <div className="date">
@@ -48,44 +50,58 @@ class ArticleView extends Component {
           </div>
           <div className="av-text">
             <p>{article.body}</p>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">Read More...</a>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+              Read More...
+            </a>
           </div>
         </div>
         <div>
           <h3>Related Articles</h3>
           <div className="av-related-articles">
-          {relatedArticles.map((article, index) => {
-            return <RelatedArticle key={index} article={article} />
-          })}
-              
+            {relatedArticles.map((article, index) => {
+              return <RelatedArticle key={index} article={article} />;
+            })}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 class ArticleConcept extends Component {
   render() {
-    let concept = this.props.concept
+    let concept = this.props.concept;
     return (
       <div>
-        <a href={concept.uri} target="_blank" rel="noopener noreferrer"> {concept.label.eng} </a>
+        <a href={concept.uri} target="_blank" rel="noopener noreferrer">
+          {" "}
+          {concept.label.eng}{" "}
+        </a>
       </div>
-    )
+    );
   }
 }
 
 class RelatedArticle extends Component {
   render() {
-    let article = this.props.article
+    let article = this.props.article;
+    let author =
+      article.authors.length > 0 ? "By " + article.authors[0].name : null;
     return (
-      <div>
-        Title: {article.title} 
+      <div className="related-article">
+        <div className="ra-title">{article.title}</div>
+        <div className="ra-sentiment">{article.sentiment}</div>
+        <div className="ra-concepts">
+          {article.concepts.slice(0, 4).map((concept, index) => {
+            return <ArticleConcept key={index} concept={concept} />;
+          })}
+        </div>
+        <div className="ra-author">
+          {author}
+        </div>
       </div>
-    )
+    );
   }
-
 }
 
 export default ArticleView;
