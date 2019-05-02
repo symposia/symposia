d4753d64aa2afe1bd4d599d8ecd7f29e93856cbb
Leandro Solidum <solidum.leandro@gmail.com>
2019-05-02 04:33:31 -0700

    start adding data to article view

--- src/components/ArticleView.js
+++ src/components/ArticleView.js
@@ -7,10 +7,46 @@ class ArticleView extends Component {
   }
 
   render() {
+    let article = this.props.article;
     return (
       <div className="article-view-container">
-        <button onClick={this.props.exitView}> Back </button>
-        <p>article view</p>
+        <div className="article-view-exit-container">
+          <button onClick={this.props.exitView}> Back </button>
+        </div>
+        <div className="article-view-main">
+          <div className="av-main-header">
+            <div className="av-news-source">
+              <p>logo here</p>
+            </div>
+            <div className="av-title">
+              <p>title here</p>
+            </div>
+          </div>
+          <div className="av-image">
+            <p>image here</p>
+          </div>
+          <div className="av-details">
+            <div className="author">
+              <p>authoer here</p>
+            </div>
+            <div className="concepts">
+              <p>concepts here</p>
+            </div>
+            <div className="date">
+              <p>date here</p>
+            </div>
+            <div className="sentiment">
+              <p>sentiment here</p>
+            </div>
+          </div>
+          <div className="av-text">
+            <p>{article.body}</p>
+            <a href={article.url} target="_blank">Read More...</a>
+          </div>
+        </div>
+        <div>
+          <p>sidebar</p>
+        </div>
       </div>
     )
   }
